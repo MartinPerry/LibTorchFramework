1d5a918282c4709af13eb59a3d48ac02a78a5aa2
Perry <info@perry.cz>
2025-10-20 18:02:43 +0200

    - update

--- LibTorchFramework/main.cpp
+++ LibTorchFramework/main.cpp
@@ -157,12 +157,12 @@ int main()
     //loaderSets.subsetSize = 200;
 
     auto ilw = std::make_shared<InputLoadersWrapper>(imSize);
-    ilw->InitLoaders<SegmentationInputLoader, std::string>({{ RunMode::TRAIN, loaderSets }}, "D:\\Datasets\\Skyfinder");
+    ilw->InitLoaders<SegmentationInputLoader, std::string>({{ RunMode::TRAIN, loaderSets }}, "E:\\Datasets\\Skyfinder");
 
     //auto trainLoader = ilw->GetLoader(RunMode::TRAIN);
     
-    auto m = std::make_shared<ModelZoo::unet::UNetModel>(imSize.channels, 1, imSize.width, imSize.height);
-    //auto m = std::make_shared<ModelZoo::u2net::U2NetModel>(imSize.channels, 1);
+    //auto m = std::make_shared<ModelZoo::unet::UNetModel>(imSize.channels, 1, imSize.width, imSize.height);
+    auto m = std::make_shared<ModelZoo::u2net::U2NetModel>(imSize.channels, 1);
         
     m->CreateOptimizer<torch::optim::Adam>(torch::optim::AdamOptions(0.0001));
 
